c8fb9609db62a5650d776b424a2575bff32a89fb
BaisakKG <baisak40@gmail.com>
2017-12-08 10:05:07 +0600

    Rename value p

--- bars.py
+++ bars.py
@@ -37,9 +37,9 @@ def get_smallest_bar(json_content):
             print_bars(bar_attributes)
 
 def distance(lat1, lon1, lat2, lon2):
-    p = 0.017453292519943295
-    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) * (1-cos((lon2-lon1)*p)) / 2
-    return 12742 * asin(sqrt(a))
+    degree = 0.017453292519943295
+    distance2dot = 0.5 - cos((lat2-lat1)*degree)/2 + cos(lat1*degree)*cos(lat2*degree) * (1-cos((lon2-lon1)*degree)) / 2
+    return 12742 * asin(sqrt(distance2dot))
 
 def get_closest_bar(json_content, latitude, longitude):
     all_coordinates = []
@@ -51,7 +51,7 @@ def get_closest_bar(json_content, latitude, longitude):
         all_coordinates.append(coordinate)
     
     human_coordinates = {'lat': float(latitude), 'lon': float(longitude)}
-    closest_bar = min(all_coordinates, key=lambda p: distance(human_coordinates['lat'], human_coordinates['lon'], p['lat'], p['lon']))
+    closest_bar = min(all_coordinates, key=lambda bar_coordinate: distance(human_coordinates['lat'], human_coordinates['lon'], bar_coordinate['lat'], bar_coordinate['lon']))
   
     print('************************** Closest bar ***********************')
     for bar_count in range(len(json_content['features'])):
@@ -64,8 +64,8 @@ if __name__ == '__main__':
     json_content = load_data(r'd:\PythonScript\Devman\bars.json')
     big_bars = get_biggest_bar(json_content)
     smallest_bars = get_smallest_bar(json_content)
-    latitude = input('Введите координаты latitude: ')
-    longitude = input('Введите координаты longitude: ')
-    get_closest_bar(json_content, latitude, longitude)    
+    # latitude = input('Введите координаты latitude: ')
+    # longitude = input('Введите координаты longitude: ')
+    # get_closest_bar(json_content, latitude, longitude)    
 
 
